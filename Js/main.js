ad243840123eec6d9753802a18db01016de4261f
Loginova Alexandra <108992189+Dukessa@users.noreply.github.com>
2023-12-27 14:22:33 +0300

    Update main.js

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     addEventListener('click', function (element) {
-      if (element.target.className !== 'burger__line' && element.target.className !== 'burger' && element.target.className !== 'menu-hero' && element.target.className !== 'menu__box') {
+      if (element.target.className !== 'burger__line' && element.target.className !== 'burger') {
         document.querySelector(".menu-hero").classList.remove("menu--js");
         document.querySelector(".burger").classList.remove("burger--js");
       }
@@ -10,10 +10,4 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector(".burger").classList.toggle("burger--js");
       document.querySelector(".menu-hero").classList.toggle("menu--js");
     })
-  
-    document.querySelector(".close").addEventListener('click', function () {
-      document.querySelector(".burger").classList.remove("burger--js");
-      document.querySelector(".menu-hero").classList.remove("menu--js");
-    })
-  
   })
